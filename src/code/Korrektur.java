68d7136b155cb635d85214947b8814e52ce29089
its-nico <nicosl@web.de>
2020-06-07 00:05:48 +0200

    Autokorrektur-Methoden fertig, es fehlt noch die IP-Abfrage bzw. die Überprüfung der Korrektheit

--- src/code/Korrektur.java
+++ src/code/Korrektur.java
@@ -1,4 +1,32 @@
 package code;
 
 public class Korrektur {
+
+ //   public static void main(String[] args) {
+ //       nullen("aa");
+ //   }
+
+    public String nullen(String text) {
+        text = text.replace("o", "0");
+        text = text.replace("O", "0");
+        return text;
+    }
+
+    public String leerzeichen(String text) {
+        text = text.replace(" ", "");
+        return text;
+    }
+
+    public String bindestriche(String text){
+        text = text.replace("-",":");
+        return text;
+    }
+
+   /* public String istIP(String text) {
+
+    }
+
+    public boolean istKorrekt(String text) {
+
+    } */
 }
