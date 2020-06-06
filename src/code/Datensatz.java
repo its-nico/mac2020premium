edfdabaca352e9950dcb5f1e583b04614d4022d4
its-nico <nicosl@web.de>
2020-06-06 19:34:52 +0200

    Klasse Datensatz fertig Klasse List fertig Klasse Manager ergänzt

--- src/code/Datensatz.java
+++ src/code/Datensatz.java
@@ -6,10 +6,43 @@ public class Datensatz {
     private String nachname;
     private String grund;
 
-    public Datensatz(String kursstufe1, String vorname1, String nachname1, String mac1, String grund1) {
-        kursstufe = kursstufe1;
-        vorname = vorname1;
-        nachname = nachname1;
-        grund = grund1;
+    public Datensatz(String pKursstufe, String pVorname, String pNachname, String pMac, String pGrund) {
+        kursstufe = pKursstufe;
+        vorname = pVorname;
+        nachname = pNachname;
+        grund = pGrund;
     }
+
+    public String getKursstufe() {
+        return kursstufe;
+    }
+
+    public void setKursstufe(String kursstufe) {
+        this.kursstufe = kursstufe;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getGrund() {
+        return grund;
+    }
+
+    public void setGrund(String grund) {
+        this.grund = grund;
+    }
+
 }
--- src/code/Manager.java
+++ src/code/Manager.java
@@ -1,4 +1,23 @@
 package code;
+import java.lang.*;
+import java.io.*;
+import java.util.*;
 
 public class Manager {
+    private static final List<Datensatz> list = new List<Datensatz>();
+
+    public static void main(String[] args) {
+        Datensatz daten1 = new Datensatz("test","test","test","test","test");
+        Datensatz daten2 = new Datensatz("test","test","test2","test","test");
+        list.insert(daten1);
+        list.insert(daten2);
+
+        String listString = (list.getContent()).getKursstufe();
+
+        System.out.println(listString);
+    }
+
+
+
+
 }
