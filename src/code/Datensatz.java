1399dff24dd0360bf80baf5050c67bd4264dfcea
its-nico <nicosl@web.de>
2020-06-06 18:55:50 +0200

    Klasse Datensatz erstellt und Klasse List neu hinzugefügt

--- src/code/Datensatz.java
+++ src/code/Datensatz.java
@@ -1,4 +1,15 @@
 package code;
 
 public class Datensatz {
+    private String kursstufe;
+    private String vorname;
+    private String nachname;
+    private String grund;
+
+    public Datensatz(String kursstufe1, String vorname1, String nachname1, String mac1, String grund1) {
+        kursstufe = kursstufe1;
+        vorname = vorname1;
+        nachname = nachname1;
+        grund = grund1;
+    }
 }
