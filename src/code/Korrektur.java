6214f9f72d3dd6c078dc55600e794e9bc7baf632
its-nico <nicosl@web.de>
2020-06-07 01:38:18 +0200

    Methoden Korrektur.istIP und Korrektur.istKorrekt programmiert

--- src/code/Korrektur.java
+++ src/code/Korrektur.java
@@ -1,28 +1,29 @@
 package code;
+import java.util.regex.*;
 
 public class Korrektur {
 
- //   public static void main(String[] args) {
- //       nullen("aa");
- //   }
+    public static void main(String[] args) {
+
+    }
 
     /* Klasse führt automatisch alle Korrektur-Methoden auf einmal aus */
     public String autoKorrektur(String text){
 
         if (istIP(text)) {
+            System.err.println("Es handelt sich hierbei um eine IP-Adresse. Bitte geben Sie eine MAC-Adresse ein!");
             return null;
         }
 
         if (istKorrekt(text)){
             return text;
         }
-
-        else{
+        else {
             return (o(leerzeichen(bindestriche(text))));
         }
     }
 
-    /* Alle Os (gro?- und kleingeschrieben) werden mit Nullen ersetzt */
+    /* Alle Os (groß- und kleingeschrieben) werden mit Nullen ersetzt */
     public String o(String text) {
         text = text.replace("o", "0");
         text = text.replace("O", "0");
@@ -43,11 +44,13 @@ public class Korrektur {
 
     /* Überprüfung, ob es sich um eine IP-Adresse statt einer MAC-Adresse handelt */
     public boolean istIP(String text) {
-    return true;
+
+        return (Pattern.matches("[^a-z^A-Z]", text)) && text.contains(".") && !text.contains(":") && !text.contains(" ");
     }
 
-    /* Überprüfung, ob die MAC-Adresse korrekt ist*/
+    /* Überprüfung, ob die MAC-Adresse korrekt ist */
     public boolean istKorrekt(String text) {
-    return true;
+
+        return !text.contains("o") && !text.contains("O") && !text.contains("-") && !text.contains(" ") && !istIP(text);
     }
 }
