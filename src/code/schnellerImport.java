268ddbd463adc02c26aee85d547bf48069703a44
its-nico <nicosl@web.de>
2020-06-26 09:17:33 +0200

    Methoden aus schnellerImport kommentiert

--- src/code/schnellerImport.java
+++ src/code/schnellerImport.java
@@ -15,7 +15,7 @@ public class schnellerImport {
 
     private static String IPString;
     private static String FormatString;
-    private static final String test = "";
+    private static final String test = ""; //"leerer" String wird später zum Verlgleichen genutzt
 
     private boolean bereitsImportiert = false;
 
@@ -26,8 +26,8 @@ public class schnellerImport {
 
     public static void schnellerImport(String importfilepath, ArrayList liste) {
 
-        IPString = "";
-        FormatString = "";
+        IPString = ""; //Auf diesen String werden später die IP-Adressen aufaddiert
+        FormatString = ""; //Auf diesen String werden später die MAC-Adressen, die nicht im korrekten Format sind, aufaddiert
 
         FileReader fr = null;
         try {
@@ -42,40 +42,40 @@ public class schnellerImport {
         String line = "";
         while (line != null) {
             try {
-                line = br.readLine(); /* Jeweils ein komplette Zeile, also ein Datensatz, wird über die Variable 'line' gespeichert */
+                line = br.readLine(); // Jeweils ein komplette Zeile, also ein Datensatz, wird über die Variable 'line' gespeichert
             } catch (IOException e) {
                 e.printStackTrace();
             }
             if (line != null) {
-                splitline(line); /* Die einzelnen Datensätze (lines) werden in ihre 5 Attribute aufgeteilt */
-                korrektur.logErstellen(mac);
-                if (korrektur.istIP(mac)){
-                    IPString = "<html>" + IPString + "<br>" + mac + "</html>";
+                splitline(line); // Die einzelnen Datensätze (lines) werden in ihre 5 Attribute aufgeteilt
+                korrektur.logErstellen(mac); //Der Kopf des Logs wird erstellt
+                if (korrektur.istIP(mac)){ //Handelt es sich um eine IP?
+                    IPString = "<html>" + IPString + "<br>" + mac + "</html>"; //Alle IP-Adressen werden aufaddiert, um dann gemeinsam aufaddiert werden zu können
                 }
                 else {
-                    mac = korrektur.autoKorrektur(mac);
-                    if (korrektur.format(mac)) {
-                        datensatz = new Datensatz(kursstufe, nachname, vorname, mac, grund);
+                    mac = korrektur.autoKorrektur(mac); //Falls keine IP wird die Adresse autokorrigierrt
+                    if (korrektur.format(mac)) { //Ist die Adresse im korrekten Format?
+                        datensatz = new Datensatz(kursstufe, nachname, vorname, mac, grund); //Falls ja wird ein neuer Datensatz mit den Daten erstellt
                         liste.add(datensatz);
                     }
                     else {
-                        FormatString = "<html>" + FormatString + "<br>" + mac + "</html>";
+                        FormatString = "<html>" + FormatString + "<br>" + mac + "</html>"; //Alle Adressen im falschen Format werden aufaddiert
                     }
 
                 }
             }
         }
 
-        if (!IPString.equals(test)){
-            dialogfenster.keineMacAdresse(IPString);
+        if (!IPString.equals(test)){ //Ist der String mit den falschen IPs nicht mehr leer?
+            dialogfenster.keineMacAdresse(IPString); //Die falschen Adressen werden in einem Dialogfenster ausgegeben
         }
-        if (!FormatString.equals(test)){
-            dialogfenster.falschesFormat(FormatString);
+        if (!FormatString.equals(test)){ //Ist der String mit den falschen MACs nicht mehr leer?
+            dialogfenster.falschesFormat(FormatString); //Die falschen Adressen werden in einem Dialogfenster ausgegeben
         }
     }
 
     public static void splitline(String line) {
-        splittedline = line.split("; "); /* Die Methode 'split' teilt den String 'line' mithilfe des definierten Trennzeichens '; '*/
+        splittedline = line.split("; "); // Die Methode 'split' teilt den String 'line' mithilfe des definierten Trennzeichens '; '
         kursstufe = splittedline[0];
         nachname = splittedline[1];
         vorname = splittedline[2];
